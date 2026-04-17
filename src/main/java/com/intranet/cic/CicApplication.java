d07ae47c65e2a65f75e29a3e726b37f75e7dc69b
thinethwic <134825739+thinethwic@users.noreply.github.com>
2026-04-17 15:22:57 +0530

    Initial commit

---
+++ src/main/java/com/intranet/cic/CicApplication.java
@@ -0,0 +1,13 @@
+package com.intranet.cic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicApplication.class, args);
+	}
+
+}
