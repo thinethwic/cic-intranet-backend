8da682d3372a8684a0177c155d5176c05bc5da44
thinethwic <134825739+thinethwic@users.noreply.github.com>
2026-05-14 09:17:30 +0530

    Edit WebConfig File

--- src/main/java/com/intranet/cic/configs/WebConfig.java
+++ src/main/java/com/intranet/cic/configs/WebConfig.java
@@ -1,28 +1,28 @@
 package com.intranet.cic.configs;
 
-import org.springframework.beans.factory.annotation.Value;
+// org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${file.upload-dir.documents}")
-    private String documentUploadDir;
-
-    @Value("${file.upload-dir.images}")
-    private String imageUploadDir;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/documents/**")
-                .addResourceLocations("file:" + documentUploadDir + "/");
-
-        registry.addResourceHandler("/uploads/images/**")
-                .addResourceLocations("file:" + imageUploadDir + "/");
-    }
+//    @Value("${file.upload-dir.documents}")
+//    private String documentUploadDir;
+//
+//    @Value("${file.upload-dir.images}")
+//    private String imageUploadDir;
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/uploads/documents/**")
+//                .addResourceLocations("file:" + documentUploadDir + "/");
+//
+//        registry.addResourceHandler("/uploads/images/**")
+//                .addResourceLocations("file:" + imageUploadDir + "/");
+//    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
